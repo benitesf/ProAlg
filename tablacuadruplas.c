226921ae32d4582fa91800d173a94a2c9a941ea4
edsonbf <edzon_bf@hotmail.com>
2016-12-21 00:32:49 +0100

    Rediseño y mejoras de las tabla de cuadruplas

--- tablacuadruplas.c
+++ tablacuadruplas.c
@@ -1,33 +1,117 @@
 #include "tablacuadruplas.h"
 
-/* Devuelve una nueva tabla */
-TablaQuad *nuevaTablaQuad(void){
 
-	TablaQuad *tabla;
-	tabla->quads = malloc(ROWS*sizeof(int*));
-	
-	int i;
-	for(i=0; i<ROWS; i++){
-		tabla->quads[i] = malloc(COLS*sizeof(int));
+/**
+ * @return: tabla de cuadruplas vacía
+ */
+quad_table new_qt()
+{
+	quad_table qt = malloc(sizeof(quad_table));
+
+	if(qt == NULL)
+	{
+		fprintf(stderr, "failed to allocate memory.\n");
+        exit(-1);
 	}
 
-	tabla->nextQuad = 0;
-	return tabla;
+	qt->first = NULL;
+	qt->last  = NULL;
+	qt->size  = 0;
+
+	return qt;
+}
+
+
+/**
+ * Inserta una cuadrupla en la tabla de cuadruplas
+ * @param qt: tabla de cuadruplas
+ * @param q:  cuadrupla a insertar
+ */
+void gen(quad_table qt, quad *q)
+{
+	quad *aux = qt->last;
+	aux->next = q;	
+	qt->last = q;
+}
+
+
+/**
+ * @return: devuelve una cuadrupla
+ */
+quad* create_new_quad(int operator, int operand1, int operand2, int result)
+{
+	quad *q = malloc(sizeof(quad));
+
+	if(q == NULL)
+	{
+		fprintf(stderr, "failed to allocate memory.\n");
+        exit(-1);
+	}
+
+	q->operator = operator;
+	q->operand1 = operand1;
+	q->operand2 = operand2;
+	q->result   = result;
+
+	return q;
+}
+
+
+/**
+ * @return: devuelve el índice de la siguiente cuadrupla
+ */
+int next_quad(quad_table qt)
+{
+	return qt->size+1;
+}
+
+/**
+ * @return: devuelve una lista con id de quad 
+ */
+list makelist(int quad)
+{
 
 }
 
-/* */
-void gen(TablaQuad *tabla, int oper, int var1, int var2, int result){
-	if (tabla->nextQuad < ROWS){
-		tabla->quads[tabla->nextQuad][0] = oper;
-		tabla->quads[tabla->nextQuad][1] = var1;
-		tabla->quads[tabla->nextQuad][2] = var2;
-		tabla->quads[tabla->nextQuad][3] = result;
-		tabla->nextQuad++;
-	}
-	else{
-		printf("Error: Llena.\n");
-	}
+
+/**
+ * @param l1: primera lista
+ * @param l2: segunda lista
+ * @return: devuelve una lista que contiene los elementos de l1 y l2
+ */
+list merge(list l1, list l2)
+{
+
 }
+
+
+/**
+ * Completa las quadruplas de la lista para que salten a la cuadrupla especificada
+ * @param l: lista
+ * @param quad: id de quad 
+ */
+void backpatch(list l, int quad)
+{
+
+}
+
+
+/**
+ * Comprueba si la tabla de cuadruplas esta vacía
+ */
+int is_empty_qt(quad_table qt)
+{
+
+}
+
+
+/**
+ * Imprime el contenido de la tabla de cuadruplas por pantalla 
+ */
+void to_string_qt(quad_table qt)
+{
+
+}
+
 
 
--- tablacuadruplas.h
+++ tablacuadruplas.h
@@ -1,20 +1,102 @@
+/*
+	@descripcion En esta cabecera se define la estructura la tabla de cuadruplas.
+	Definimos una cuadrupla con una estructura compuesta de la siguiente manera
+	<operator, operand1, operand2, result> donde:
+		-operator: es un operador
+		-operand1: es el primer operador
+		-operand2: es el segundo operador
+		-result:   es el resultado de la operación
+		-next:     es un puntero a la siguiente cuadrupla
+	
+	@autor Benites Fernández, Edson	        
+*/
+
 #ifndef CB_TABLACUADRUPLAS_H
 #define CB_TABLACUADRUPLAS_H
-#define ROWS 100
-#define COLS 4
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include "util/list.c"
 
-typedef struct TablaQuad{
+/* Se define la estructura la cuadrupla */
+typedef struct quad{
+	int operator;
+	int operand1;
+	int operand2;
+	int result;
+	struct quad *next;
+}quad;
 
-	int **quads;
-	int nextQuad;
+/* Se define la estructura la tabla de cuadruplas */
+struct quad_table{
+	quad *first;
+	quad *last;
+	int size;
+};
 
-} TablaQuad;
+/* Se define el tipo quad_table como un puntero a la estructura quad_table */
+typedef struct quad_table* quad_table;
 
-TablaQuad *nuevaTablaQuad(void);
-void gen(TablaQuad *tabla, int oper, int var1, int var2, int result);
+/////////////////////////////////////////
+
+/**
+ * @return: tabla de cuadruplas vacía
+ */
+quad_table new_qt();
+
+
+/**
+ * Inserta una cuadrupla en la tabla de cuadruplas
+ * @param qt: tabla de cuadruplas
+ * @param q:  cuadrupla a insertar
+ */
+void gen(quad_table qt, quad *q);
+
+
+/**
+ * @return: devuelve una cuadrupla
+ */
+quad* create_new_quad(int operator, int operand1, int operand2, int result);
+
+
+/**
+ * @return: devuelve el índice de la siguiente cuadrupla
+ */
+int next_quad(quad_table qt);
+
+/**
+ * @return: devuelve una lista con id de quad 
+ */
+list makelist(int quad);
+
+
+/**
+ * @param l1: primera lista
+ * @param l2: segunda lista
+ * @return: devuelve una lista que contiene los elementos de l1 y l2
+ */
+list merge(list l1, list l2);
+
+
+/**
+ * Completa las quadruplas de la lista para que salten a la cuadrupla especificada
+ * @param l: lista
+ * @param quad: id de quad 
+ */
+void backpatch(list l, int quad);
+
+
+/**
+ * Comprueba si la tabla de cuadruplas esta vacía
+ */
+int is_empty_qt(quad_table qt);
+
+
+/**
+ * Imprime el contenido de la tabla de cuadruplas por pantalla 
+ */
+void to_string_qt(quad_table qt);
+
 
 #endif
